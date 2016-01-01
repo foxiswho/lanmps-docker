20edb7c99b23021318f419c98e9817be9c5ea415
fox <80005368@qq.com>
2016-01-01 14:56:05 +0800

    优化

--- start.sh
+++ start.sh
@@ -3,5 +3,8 @@
 # Disable Strict Host checking for non interactive git clones
 
 
+
+mkdir -p /www/wwwroot/default/logs
+ln -s /www/wwwLogs/* /www/wwwroot/default/logs/
 # Start supervisord and services
 /usr/bin/supervisord -n -c /etc/supervisord.conf

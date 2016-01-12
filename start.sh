81de9ea6d86ca310c7e4d90af3a25ff4a72a09c0
fox <opp506@qq.com>
2016-01-12 13:51:10 +0800

    log

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ if [ ! -f /www/wwwroot/vhost/more.yes ]; then
     ln -s /www/wwwroot/wwwLogs/localhost.log /www/wwwroot/default/logs/localhost.log
     ln -s /www/wwwroot/wwwLogs/nginx_error.log /www/wwwroot/default/logs/nginx_error.log
     ln -s /www/wwwroot/wwwLogs/php-fpm.log /www/wwwroot/default/logs/php-fpm.log
+    ln -s /www/wwwroot/wwwLogs/supervisord.log /www/wwwroot/default/logs/supervisord.log
 fi
 
 

3cac3f62080f09a67665537576a261ada1b379f7
fox <foxiswho@gmail.com>
2016-01-22 10:53:21 +0800

    wenjian

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 mkdir -p /www/wwwroot/default/logs
 chown -R www:www /www/wwwroot/default/logs
 chmod -R 777  /www/wwwroot/default/logs
+echo " START ==================================\n" >> /www/wwwroot/default/logs/supervisord.log
 
 /etc/init.d/supervisor start
 

941b854c63e03cb32772db9dfe7542742388aff6
fox <foxiswho@qq.com>
2016-01-23 11:15:36 +0800

    复制配置文件

--- start.sh
+++ start.sh
@@ -10,11 +10,14 @@ chmod -R 777  $WWW_ROOT
 touch $WWW_ROOT/vhost/logs/index.log
 echo " START ==================================\n" >> $WWW_ROOT/vhost/logs/supervisord.log
 
-CONF=/www/lanmps/nginx/conf/vhost/00000.default.conf
+CONF=$WWW_ROOT/vhost/00000.default.conf
 if [ ! -f $CONF ]; then
-    cp -auvR $CONF $WWW_ROOT/vhost/
+    cp -auvR /www/lanmps/nginx/conf/vhost/00000.default.conf $WWW_ROOT/vhost/
 fi
-
+INDEX=$WWW_ROOT/index.php
+if [ ! -f $INDEX ]; then
+    echo "<?php\n phpinfo();" > $INDEX
+fi
 
 /etc/init.d/supervisor start
 
